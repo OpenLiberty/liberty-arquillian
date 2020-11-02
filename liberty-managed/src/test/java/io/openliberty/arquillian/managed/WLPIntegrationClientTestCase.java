4cd4883258e2b395e9c1cb92e1b866480673ed8c
Kathryn Kodama <kathryn.s.kodama@gmail.com>
2020-11-02 12:54:44 -0500

    fixed formatting and code clean up (#81)
    Signed-off-by: Kathryn Kodama <kathryn.s.kodama@gmail.com>

--- liberty-managed/src/test/java/io/openliberty/arquillian/managed/WLPIntegrationClientTestCase.java
+++ liberty-managed/src/test/java/io/openliberty/arquillian/managed/WLPIntegrationClientTestCase.java
@@ -1,17 +1,17 @@
-// /*
-//  * Copyright 2010-2012, Red Hat Middleware LLC, and individual contributors
-//  * identified by the Git commit log. 
-//  *
-//  * Licensed under the Apache License, Version 2.0 (the "License");
-//  * you may not use this file except in compliance with the License.
-//  * You may obtain a copy of the License at
-//  * http://www.apache.org/licenses/LICENSE-2.0
-//  * Unless required by applicable law or agreed to in writing, software
-//  * distributed under the License is distributed on an "AS IS" BASIS,
-//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  * See the License for the specific language governing permissions and
-//  * limitations under the License.
-//  */
+/*
+ * Copyright 2010-2012, Red Hat Middleware LLC, and individual contributors
+ * identified by the Git commit log. 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.openliberty.arquillian.managed;
 
 import java.io.ByteArrayOutputStream;
@@ -31,8 +31,8 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-// // This is based on the JettyEmbeddedClientTestCase.java
-// // written by Dan Allen and Aslak Knutsen.
+// This is based on the JettyEmbeddedClientTestCase.java
+// written by Dan Allen and Aslak Knutsen.
 
 @RunWith(Arquillian.class)
 public class WLPIntegrationClientTestCase
